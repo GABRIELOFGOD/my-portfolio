bbec94a297b92cc2136b46bb59d3e69c90bbdaa9
GABRIELOFGOD <114562466+GABRIELOFGOD@users.noreply.github.com>
2022-11-07 15:37:27 +0100

    Delete Gabriel/simple_data directory

--- Gabriel/simple_data/main.py
+++
@@ -1,7 +0,0 @@
-class Data:
-    def Data(self, name, project):
-        self.name = name
-        self.project = project
-
-        name = input('What\'s your name?: ')
-        project = 'Data Structure'
